69254954d401ef90ad48fb0be13d3c3c2c125f6f
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-10-11 23:55:26 +0200

    added healthbar

---
+++ scripts/fill_bar.gd
@@ -0,0 +1,32 @@
+class_name FillBar
+extends ProgressBar
+
+@export var max_fill: float = 100;
+@export var current_fill: float = 100;
+
+signal is_depleted()
+
+func _process(delta: float) -> void:
+	decrease_health(1)
+
+func set_data(curr_health: float = max_fill):
+	current_fill = curr_health
+	
+	if has_theme_stylebox("background"):
+		var value_padding_left = get_theme_stylebox("background").get("border_width_left") * 2;
+		var value_padding_right = get_theme_stylebox("background").get("border_width_right") * 2;
+		max_value = max_fill + value_padding_left + value_padding_right;
+	
+	mouse_filter = Control.MOUSE_FILTER_IGNORE;
+	value = current_fill;
+	visible = true;
+
+func decrease_health(amount: float):
+	current_fill -= amount;
+	value = current_fill;
+	
+	if current_fill <= 0:
+		is_depleted.emit()
+		
+func is_empty():
+	return current_fill > 0;
