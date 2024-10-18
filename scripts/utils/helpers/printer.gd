d0483f293718dc136924c42c4655100d85c829fe
Torben Van Assche <38832704+torbenvanassche@users.noreply.github.com>
2024-10-18 14:53:37 +0200

    added text container to print dialogue

--- scripts/utils/helpers/printer.gd
+++ scripts/utils/helpers/printer.gd
@@ -1,11 +1,12 @@
 class_name Printer
-extends Label
+extends Control
 
 var print_tween: Tween
 var _is_printing = false
 
 @export var print_delay: float = 0.5
 @onready var finished_indicator: TextureRect = $Sprite2D;
+@onready var text_box: Label = $MarginContainer2/text_box;
 
 func _ready():
 	visible = false;
@@ -13,18 +14,18 @@ func _ready():
 func print(txt: String):
 	_is_printing = true
 	visible = true;
-	self.visible_characters = 0
-	self.text = txt;
+	text_box.visible_characters = 0
+	text_box.text = txt;
 	
 	print_tween = create_tween()
-	print_tween.tween_property(self, "visible_characters", txt.length(), print_delay * txt.length())
+	print_tween.tween_property(text_box, "visible_characters", txt.length(), print_delay * txt.length())
 	print_tween.finished.connect(_on_tween_complete)
 	
 func _input(_event):
 	if Input.is_action_just_pressed("primary_click"):
 		if _is_printing:
 			print_tween.stop();
-			visible_ratio = 1;
+			text_box.visible_ratio = 1;
 			print_tween.finished.emit()
 		else:
 			visible = false;
