dd06cc9e7fec5a0acc67e98996efb1eef4fcf564
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-10-19 00:06:22 +0200

    fixed camera default position not being correctly initialized

--- scripts/utils/helpers/camera.gd
+++ scripts/utils/helpers/camera.gd
@@ -2,21 +2,27 @@ extends Camera3D
 
 @export var target: Node3D
 @export var follow_speed: float = 1.0
+@export var rotation_speed: float = 0.25;
 
 var initial_offset: Vector3
-var initial_rotation: Quaternion
+var initial_rotation: Quaternion;
+var rotation_difference: Quaternion;
 
 func _ready():
 	Manager.instance.camera = self;
 	
 	if target:
 		initial_offset = global_transform.origin - target.global_transform.origin
+		
 		initial_rotation = global_transform.basis.get_rotation_quaternion()
+		look_at(target.global_transform.origin, Vector3.UP)
+		var new_rotation = global_transform.basis.get_rotation_quaternion()
+		rotation_difference = initial_rotation.inverse() * new_rotation;
 
 func _process(delta):
 	if target:
 		var target_position = target.global_transform.origin + initial_offset
 		global_transform.origin = global_transform.origin.lerp(target_position, delta * follow_speed)
 
-		look_at(target.global_transform.origin, Vector3.UP)
-		global_transform.basis = Basis(initial_rotation) * global_transform.basis
+		look_at(target.global_transform.origin + (global_transform.origin - target_position) * rotation_speed, Vector3.UP)
+		global_transform.basis *= Basis(rotation_difference)
