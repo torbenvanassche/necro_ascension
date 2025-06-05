53df1e1adbb0004b055039799f9f3c498a78f18e
Torben Van Assche <torbenvanassche.anime@gmail.com>
2025-06-06 00:20:02 +0200

    working on body part manager

--- scripts/body_parts/body_part_donor.gd
+++ scripts/body_parts/body_part_donor.gd
@@ -1,3 +1,9 @@
 class_name BodyPartDonor extends Node3D
 
-@export var arm_left: MeshInstance3D;
+@export var meshes: Dictionary[String, MeshInstance3D];
+
+func get_part(part_name: String) -> MeshInstance3D:
+	if meshes.has(part_name):
+		return meshes[part_name];
+	Debug.err("%s was not found on BodyPartDonor" % part_name)
+	return null;
--- scripts/body_parts/body_part_receiver.gd
+++ scripts/body_parts/body_part_receiver.gd
@@ -4,26 +4,18 @@ extends Skeleton3D
 @export var donor: PackedScene
 @export var arm_left: MeshInstance3D
 
-func _ready() -> void:
-	var part_donor := donor.instantiate() as BodyPartDonor
+func replace_mesh_instance(to_remove: MeshInstance3D, to_add: MeshInstance3D) -> void:
+	var original_global_transform := to_remove.global_transform
 
-	# Store transform of current arm before replacing
-	var original_global_transform := arm_left.global_transform
+	if is_instance_valid(to_remove) and to_remove.get_parent() == self:
+		remove_child(to_remove)
+		to_remove.queue_free()
 
-	# Remove the old arm if it's still in the scene
-	if is_instance_valid(arm_left) and arm_left.get_parent() == self:
-		remove_child(arm_left)
-		arm_left.queue_free()
+	if is_instance_valid(to_add) and to_add.get_parent():
+		to_add.get_parent().remove_child(to_add)
 
-	# Prepare the new donor arm
-	var donor_arm := part_donor.arm_left
-	donor_arm.get_parent().remove_child(donor_arm)
+	add_child(to_add)
+	to_add.owner = self
+	to_add.skeleton = get_path()
 
-	# Assign and attach
-	arm_left = donor_arm
-	add_child(arm_left)
-	arm_left.owner = self
-	arm_left.skeleton = get_path()
-
-	# Restore original position
-	arm_left.global_transform = original_global_transform
+	to_add.global_transform = original_global_transform
